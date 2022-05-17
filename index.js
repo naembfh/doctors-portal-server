08af80dd9a9f780300ce91899ff7fd7d730db781
Mehedi Nayem <naembfh@gmail.com>
2022-05-18 04:10:49 +0800

    check

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function verifyJWT(req, res, next) {
     return res.status(401).send({ message: 'UnAuthorized access' });
   }
   const token = authHeader.split(' ')[1];
+  
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
     if (err) {
       return res.status(403).send({ message: 'Forbidden access' })
@@ -37,6 +38,17 @@ async function run() {
     const serviceCollection = client.db('doctors_portal').collection('services');
     const bookingCollection = client.db('doctors_portal').collection('bookings');
     const userCollection = client.db('doctors_portal').collection('users');
+    const doctorCollection = client.db('doctors_portal').collection('doctors');
+
+    const verifyAdmin=async(req,res,next)=>{
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      if(requesterAccount.role==='admin'){
+        next()
+      }else{
+        res.status(403).send({message:'forbidden'})
+      }
+    }
 
     app.get('/service', async (req, res) => {
       const query = {};
@@ -148,6 +160,23 @@ async function run() {
       }
       const result = await bookingCollection.insertOne(booking);
       return res.send({ success: true, result });
+    })
+
+app.get('/doctor',verifyJWT,verifyAdmin, async(req,res)=>{
+  const doctors=await doctorCollection.find().toArray()
+  res.send(doctors)
+})
+
+    app.post('/doctor',verifyJWT,verifyAdmin, async(req,res)=>{
+      const doctor=req.body;
+      const result=await doctorCollection.insertOne(doctor)
+      res.send(result);
+    })
+    app.delete('/doctor/:email',verifyJWT,verifyAdmin, async(req,res)=>{
+      const email=req.params.email;
+      const filter={email:email}
+      const result=await doctorCollection.deleteOne(filter)
+      res.send(result);
     })
 
   }
